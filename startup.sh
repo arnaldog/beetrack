c9d22dbde183906947fa6e26c9155c1f043b3366
Arnaldo Gaspar <arnaldog@gmail.com>
2017-03-30 18:56:07 -0300

    Initial commit

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker volume create --name beetrack-postgres
+docker volume create --name beetrack-redis
+
+docker-compose run --user "$(id -u):$(id -g)" beetrack rake db:reset
+docker-compose run --user "$(id -u):$(id -g)" beetrack rake db:migrate
+
+docker-compose up
+
+
